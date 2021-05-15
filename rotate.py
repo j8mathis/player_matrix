04fc5198e56a16e0f41234e512a770636a5ea5b8
j8mathis <jasonjmathis@gmail.com>
2021-05-15 10:34:02 -0600

    wrong file

--- rotate.py
+++ rotate.py
@@ -1,47 +1,84 @@
 import pandas as pd
 import numpy as np
+import random 
 
-df = pd.DataFrame({
-        'A':list('abcdef'),
-        'B':[1,2,3,4,5,6],
-})
+def get_line_up(player_count): 
+    if player_count == 12:
+        lineup = {
+        '1': ['pitch', 'catch','1st', '2nd', 'shortstop', '3rd', 'left', 'center', 'right', 'bench', 'bench', 'bench'],
+        '2': ['bench', 'bench', 'bench', 'right', 'center', 'shortstop', '2nd', '1st', 'pitch', 'catch', 'left', '3rd'],
+        '3': ['left','pitch','bench','bench','bench','right','center', 'shortstop', 'left', '3rd','1st', '2nd'],
+        '4': ['center','1st','3rd','2nd','bench','bench','pitch','catch','bench','left','right','shortstop'],
+        '5': ['pitch','shortstop','center','2nd','1st','catch','bench','bench','3rd','left','right','bench'],
+        '6': ['left','catch','1st','pitch','shortstop','3rd','2nd','center','right','bench','bench','catch']
+        }
 
-day_list = pd.to_datetime(['2015-01-02','2016-05-05','2015-08-09'])
-#alternative
-#day_list = pd.DatetimeIndex(['2015-01-02','2016-05-05','2015-08-09'])
+    elif player_count == 11:
+        lineup = {
+        '1': ['pitch', 'catch','1st', '2nd', 'shortstop', '3rd', 'left', 'center', 'right', 'bench', 'bench'],
+        '2': ['bench', 'bench', '3rd', 'right', 'center', 'shortstop', '2nd', '1st', 'pitch', 'catch', 'left'],
+        '3': ['left','pitch','bench','bench','2nd','right','center', 'shortstop', 'left', '3rd','1st'],
+        '4': ['center','1st','3rd','2nd','bench','bench','pitch','catch','shortstop','left','right'],
+        '5': ['pitch','shortstop','center','2nd','1st','catch','bench','bench','3rd','left','right'],
+        '6': ['left','catch','1st','pitch','shortstop','3rd','2nd','center','right','bench','catch'] 
+        }
 
-df["rand_day"] = np.random.choice(day_list, size=len(df))
+    elif player_count == 10:
+        lineup = {
+        '1': ['pitch', 'catch','1st', '2nd', 'shortstop', '3rd', 'left', 'center', 'right', 'bench'],
+        '2': ['bench', 'left', '3rd', 'right', 'center', 'shortstop', '1st', '2nd', 'pitch', 'catch'],
+        '3': ['2nd','pitch','bench','shortstop','1st','right','center','catch', 'left', '3rd'],
+        '4': ['2nd','bench','left','3rd','right','pitch','catch','shortstop','1st', 'center'],
+        '5': ['shortstop','center','pitch','catch','1st','left', 'bench','right','3rd','2nd'],
+        '6': ['left','3rd','pitch','shortstop','center','2nd','1st','right','catch', 'bench'] 
+        }
 
-df1_transposed = df.T
+    elif player_count == 9:
+        lineup = {
+        '1': ['pitch', 'catch','1st', '2nd', '3rd', 'shortstop', 'left', 'center', 'right'],
+        '2': ['3rd', 'left', 'right', 'center', 'shortstop', '1st', '2nd', 'pitch', 'catch'],
+        '3': ['left','pitch','catch','1st','2nd','center', 'shortstop', 'left', '3rd'],
+        '4': ['center','1st','2nd','3rd','right','pitch','catch','shortstop','left'],
+        '5': ['shortstop','center','pitch','catch','1st','left','right','3rd','2nd'],
+        '6': ['left','3rd','pitch','shortstop','center','2nd','1st','right','catch'] 
+        }
+    else:
+        if player_count > 12:
+            print("ERROR: You have too many players!")
+        elif player_count < 9:
+            print("ERROR: You don't have enough players!")
+        lineup = {}
 
-#print(df)
+    return lineup
 
-data = {
-    'player_1': ["pitch","bench","left","center","pitch","left"], 
-    'player_2': ["catch","bench","pitch","1st","shortstop","catch"],
-    'player_3': ["1st","bench","bench","3rd","center","1st"],
-    'player_4': ["2nd","right","bench","2nd","right","pitch"],
-    'player_5': ["shortstop","bench","bench","center","1st","shortstop"],
-    'player_6': ["3rd","shortstop","bench","shortstop","catch","3rd"],
-    'player_7': ["left","2nd","center","pitch","left","2nd"],
-    'player_8': ["center","1st","shortstop","catch","3rd","center"],
-    'player_9': ["right","pitch","left","2nd","bench","pitch"],
-    'player_10':["bench","catch","3rd","1st","pitch","catch"],
-    'player_11': ["bench","left","1st","1st","pitch","catch"],
-    'player_12': ["bench","3rd","2nd","1st","pitch","catch"]
-    }
+###################################
+no = ["Edward", "Julian"]
 
+sub = []
 
-#one = ['pitch', 'catch','1st', '2nd', 'shortstop', '3rd', 'left', 'center', 'right', 'bench', 'bench', 'bench']
-#two = ['bench', 'bench', 'bench', 'right', 'center', 'shortstop', '2nd', '1st', 'pitch', 'catch', 'left', '3rd']
-#three = ['left','pitch','bench','bench','bench','right','center', 'shortstop', 'left', '3rd','1st', '2nd']
-four = ['center','1st','3rd','2nd','bench','bench','pitch','catch','bench','left','right','shortstop']
-five = ['pitch','shortstop','center','2nd','1st','catch','bench','bench','3rd','left','right','bench']
-six = ['left','catch','1st','pitch','shortstop','3rd','2nd','center','right','bench','bench','catch']
+output_csv = False
+###################################
 
+players = ['Max', 'Grayson', 'Charlie', 'Silas', 'Tyce', 'Walter', 'Jamie', 'Edward', 'Stephen', 'Julian', 'Jack', 'Sander']
+random.shuffle(players)
 
+for i in no:
+    players.remove(i)
 
-df2 = pd.DataFrame.from_dict(data, orient='index')
-print(df2)
-# for k,v in data.items():
-#     print(k,v)
+for i in sub:
+    players.append(i)
+
+player_count = len(players)
+
+lineup = get_line_up(player_count)
+
+if lineup: 
+    df = pd.DataFrame(data=lineup)
+    idx = 0
+    df.insert(loc=idx, column='players', value=players)
+    df_csv = df.to_csv(index=False)
+    if output_csv:
+        print(df_csv)
+    else:
+        print(df)
+
